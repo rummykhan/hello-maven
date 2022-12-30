30d3dc8c127245bb4b67bdc2e84d2c867f6d6987
Rehan Manzoor <manzoorr@amazon.com>
2022-12-30 16:56:25 -0500

    Initial commit

---
+++ src/main/java/org/example/ui/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.example.ui;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorld {
+
+    public String getName(){
+        return "rummykhan";
+    }
+}
